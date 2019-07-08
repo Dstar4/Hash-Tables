b267c0ad874ab2d6a33dc2ff1198198ac94f1cdb
Daniel <danielstarling4@gmail.com>
2019-07-08 15:25:44 -0700

    day 1 complete

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -3,17 +3,17 @@ class Pair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
+        self.next_node = None
 
 
 # Basic hash table
-# Fill this in.  All storage values should be initialized to None
+#   All storage values should be initialized to None
 class BasicHashTable:
     def __init__(self, capacity):
         self.capacity = capacity
         self.storage = [None for i in range(capacity)]
 
 
-# Fill this in.
 # Research and implement the djb2 hash function
 def hash(string):
     tmp = 5381
@@ -31,48 +31,59 @@ def djb2(key):
     return hash
 """
 
-# Fill this in.
-
 
 def hash_table_insert(hash_table, key, value):
     # hash the key so you can compare the hashed keys
     hashed_key = hash(key)
     index = hashed_key % hash_table.capacity
-    # print('\nIndex', index)
-
     # Create a new node with the Pair class
     new_node = Pair(key, value)
-    # Look for an existing node with the same key
     existing_node = hash_table.storage[index]
 
     if existing_node:
-        # last_node = None
-
-        while existing_node:
-            if existing_node.key == key:
-                # If you are overwriting a key with a different value, print a warning.
-                print("You are overwriting an existing key's value.")
-                existing_node.value == value
-                return
-
-            # last_node = existing_node
-            existing_node = existing_node.next_node
-        # last_node.next_node = new_node
-
+        # Look for an existing node with the same key
+        if existing_node.key == key:
+            # If you are overwriting a key with a different value, print a warning.
+            print("You are overwriting an existing key's value.")
+            existing_node.value == value
+        existing_node = existing_node.next_node
     else:
         hash_table.storage[index] = new_node
-        # Fill this in.
-        # If you try to remove a value that isn't there, print a warning.
 
 
 def hash_table_remove(hash_table, key):
-    pass
+    # same setup as insert
+    hashed_key = hash(key)
+    index = hashed_key % hash_table.capacity
 
-
-# Fill this in.
+    existing_node = hash_table.storage[index]
+    if existing_node:
+        last_node = None
+        # while the current index exists
+        while existing_node:
+            if existing_node.key == key:
+                # if last node is not None set it to the next node
+                if last_node:
+                    last_node.next_node = existing_node.next_node
+                # else set the key
+                else:
+                    hash_table.storage[index] = existing_node.next_node
+            last_node = existing_node
+            existing_node = existing_node.next_node
+    else:
+        # If you try to remove a value that isn't there, print a warning.
+        print("Unable to remove item")
 # Should return None if the key is not found.
+        return None
+
+
 def hash_table_retrieve(hash_table, key):
-    pass
+    hashed_key = hash(key)
+    index = hashed_key % hash_table.capacity
+
+    existing_node = hash_table.storage[index]
+    if existing_node:
+        return existing_node.value
 
 
 def Testing():
