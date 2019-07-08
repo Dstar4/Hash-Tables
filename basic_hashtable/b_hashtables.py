831b952556712478c0c603ee6f0100015670d01a
Daniel <danielstarling4@gmail.com>
2019-07-08 12:28:52 -0700

    DJB2 added

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -1,54 +1,49 @@
-
-
-# '''
 # Basic hash table key/value pair
-# '''
 class Pair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
 
 
-# '''
 # Basic hash table
 # Fill this in.  All storage values should be initialized to None
-# '''
 class BasicHashTable:
     def __init__(self, capacity):
         pass
 
 
-# '''
 # Fill this in.
 # Research and implement the djb2 hash function
-# '''
+"""
+ def djb2(key):
+     hash = 5381
+     for c in key:
+         hash = (hash * 33) + ord(c)
+     return hash
+"""
+
+
 def hash(string, max):
-    pass
+    tmp = 5381
+    for c in string:
+        tmp = (tmp * 33) + ord(c)
+    return hash
 
 
-# '''
 # Fill this in.
-
 # If you are overwriting a value with a different key, print a warning.
-# '''
 def hash_table_insert(hash_table, key, value):
     pass
 
 
-# '''
 # Fill this in.
-
 # If you try to remove a value that isn't there, print a warning.
-# '''
 def hash_table_remove(hash_table, key):
     pass
 
 
-# '''
 # Fill this in.
-
 # Should return None if the key is not found.
-# '''
 def hash_table_retrieve(hash_table, key):
     pass
 
